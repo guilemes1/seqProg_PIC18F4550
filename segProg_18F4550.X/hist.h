5cf6e31c4ae71a55191fa743ce1fd75737947511
Guilherme Lemes <78973832+guilemes1@users.noreply.github.com>
2021-11-15 09:08:50 -0300

    Update hist.h

--- segProg_18F4550.X/hist.h
+++ segProg_18F4550.X/hist.h
@@ -1,42 +1,44 @@
 /*
- * 08:26 16/10/2020 - Luis - criaÁ„o do arquivo de histÛrico
- * 09:33 26/10/2020 - Luis - implementaÁ„o de passos de tempo, utilizando Timer1
- * atualizaÁ„o do codigo no qual foi feito uma limpa de funÁıes que nao estavam sendo utilizadas
- * atualizando as existentes com auxilio do professor JosÈ, deixando o codigo mais simples de ser compreendido e modificado 
- * 10:35 28/10 - Lucas V - AtualizaÁ„o. ImplementaÁ„o da rotina de repetiÁ„o de sequencias de passos. 
- * 08:22 30/11/2020 - Lucas V - ResoluÁ„o de bug no retorno da tela de ciclos.
+ * 08:26 16/10/2020 - Luis - cria√ß√£o do arquivo de hist√≥rico
+ * 09:33 26/10/2020 - Luis - implementa√ß√£o de passos de tempo, utilizando Timer1
+ * atualiza√ß√£o do codigo no qual foi feito uma limpa de fun√ß√µes que nao estavam sendo utilizadas
+ * atualizando as existentes com auxilio do professor Jos√©, deixando o codigo mais simples de ser compreendido e modificado 
+ * 10:35 28/10 - Lucas V - Atualiza√ß√£o. Implementa√ß√£o da rotina de repeti√ß√£o de sequencias de passos. 
+ * 08:22 30/11/2020 - Lucas V - Resolu√ß√£o de bug no retorno da tela de ciclos.
  * 
  *  
 */
 
-/* Projeto Sequenciador LÛgico Program·vel - Vers„o 2021
+/* Projeto Sequenciador L√≥gico Program√°vel - Vers√£o 2021
  * 
- * REALIZAR ATUALIZA«√O DO HIST”RICO DIARIAMENTE - falta implementar conteudo
+ * REALIZAR ATUALIZA√á√ÉO DO HIST√ìRICO DIARIAMENTE - falta implementar conteudo
  *
  * /09/2021 - /09/2021 - Pedro - Desenvolvimento da biblioteca EEPROM
- * /09/2021 - /09/2021 - Guilherme - Inicio do desenvolvimento da biblioteca I2C para implementaÁ„o do conversor digital-analÛgico MCP4725 - Pendente
- * /09/2021 - /09/2021 - Pedro e Carlos - Estudo para implementaÁ„o da biblioteca  
+ * /09/2021 - /09/2021 - Guilherme - Inicio do desenvolvimento da biblioteca I2C para implementa√ß√£o do conversor digital-anal√≥gico MCP4725 - Pendente
+ * /09/2021 - /09/2021 - Pedro e Carlos - Estudo para implementa√ß√£o da biblioteca  
  * 
- * Realizado modificaÁ„o da paginaÁ„o do dispositivo, a fim de propiciar uma melhora na interaÁ„o com o usuario e facilidade de de navegaÁ„o entre telas
+ * Realizado modifica√ß√£o da pagina√ß√£o do dispositivo, a fim de propiciar uma melhora na intera√ß√£o com o usuario e facilidade de de navega√ß√£o entre telas
  * 
  * 
- * 16/10/2021 - Guilherme - Inclus„o da biblioteca EEPROM ao programa principal
+ * 16/10/2021 - Guilherme - Inclus√£o da biblioteca EEPROM ao programa principal
  *                        - Desenvolvido a possibilidade de salvar e realizar o resgate de sequencias salvas na memoria
- * 18/10/2021 - Guilherme - CorreÁ„o de bug - CorreÁ„o para buscar o tempo correto inserido na sequencia extraida da memoria
+ * 18/10/2021 - Guilherme - Corre√ß√£o de bug - Corre√ß√£o para buscar o tempo correto inserido na sequencia extraida da memoria
  * 18/10/2021 -   Pedro   - Desenvolvimento de biblioteca I2C - upgrade 
- * 20/10/2021 - Guilherme - CorreÁ„o de bug - Busca de sequencias vazias na memÛria
+ * 20/10/2021 - Guilherme - Corre√ß√£o de bug - Busca de sequencias vazias na mem√≥ria
  * 21/10/2021 - Guilherme - 
  * 22/10/2021 - Guilherme - 
- * 23/10/2021 - Guilherme - Finalizado implementaÁ„o da biblioteca EEPROM, de modo a possibiitar a busca de sequencias salvas na memoria, bem como o estado inicial dos atuadores para cada sequencia desejada
-                            Para salvar as condiÁıes iniciais dos atuadores para cada sequÍncia foram desenvolvidas e utilizadas as seguintes funÁıes:
+ * 23/10/2021 - Guilherme - Finalizado implementa√ß√£o da biblioteca EEPROM, de modo a possibiitar a busca de sequencias salvas na memoria, bem como o estado inicial dos atuadores para cada sequencia desejada
+                            Para salvar as condi√ß√µes iniciais dos atuadores para cada sequ√™ncia foram desenvolvidas e utilizadas as seguintes fun√ß√µes:
  * 
  *                          char salva_atuadores_init( void );	    - Funcao criada para retornar a condicao inicial dos atuadores para cada sequencia criada
  *                          void manipula_atuadores_init(char aux); - Funcao criada para manipular os atuadores conforme a condicao inicial buscada na memoria 
  *                          void salvar_dado (char adrs, char col, unsigned char data);  - funcao criada para salvar um unico dado de qualquer endereco de memoria da EEPROM
  *                          void buscar_dado (char adrs, char col, unsigned char *data); - Funcao criada para buscar um unico dado de qualquer endereco de memoria da EEPROM
  * 
- * 23/10/2021 -   Pedro   - FinalizaÁ„o da biblioteca I2C - Testes e simulaÁ„o
+ * 23/10/2021 -   Pedro   - Finaliza√ß√£o da biblioteca I2C - Testes e simula√ß√£o
  * 
+ * 12/11/2021 - Guilherme - Realizado a troca do microcontrolador PIC16F887 pelo microcontrolador PIC18F4550, de modo a dispor de mais mem√≥ria para execu√ß√£o do programa
+ *                          * Realizado readequa√ß√£o das bibliotecas para o novo componente utilizado conforme datasheet  
  * 
  *                          
  
